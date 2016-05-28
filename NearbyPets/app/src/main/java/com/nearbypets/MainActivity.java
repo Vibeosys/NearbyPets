91ca040eccf1ad2a5cca7ddae8eef690ccb656ed
AkshayVGadhave <akshay@vibeosys.com>
2016-05-28 16:21:55 +0530

    Folder Structure and fonts are added
    Folder Structure and fonts are added

--- NearbyPets/app/src/main/java/com/nearbypets/MainActivity.java
+++ NearbyPets/app/src/main/java/com/nearbypets/MainActivity.java
@@ -23,14 +23,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_my_detail) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_my_posted_ads) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_my_saved_ads) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_view_categories) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_post_new_ad) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_log_out) {
 
         }
 
